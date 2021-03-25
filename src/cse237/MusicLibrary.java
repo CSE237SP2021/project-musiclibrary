6a9e3d49744c38cbf591d6afeed87ed53ac25e38
sangwooksuh <sangwooksuh@172.16.62.135>
2021-03-25 16:07:30 -0500

    implemented main menu in MusicLibrary issue #16 - subject to change

--- src/cse237/MusicLibrary.java
+++ src/cse237/MusicLibrary.java
@@ -18,9 +18,79 @@ public class MusicLibrary {
 	public static void main(String[] args) {
 		
 		MusicLibrary musicLibrary = new MusicLibrary(); 
-		
-		//TODO: call a method on musicLibrary to run the program (display menu & process it)
+		musicLibrary.runMenu();
 	}
 	
-
+	private void runMenu() {
+		displayMainMenu();
+		int selectedOption = this.getUserInput();
+		processMainMenu(selectedOption);
+	}
+	
+	private void displayMainMenu() {
+		System.out.println("Welcome to Music Library!");
+		playlistHelper.showPlaylists();
+			/*
+			 * Current Playlists:
+			 * 0. All Songs 
+			 * 1. Favorites
+			 * 2. <custom playlist name here>
+			 * ...
+			 */
+		System.out.println("Please select an option: ");
+		System.out.println("1. Play an existing playlist");
+		System.out.println("2. Edit an existing playlist");
+		System.out.println("3. Delete an existing playlist");
+		System.out.println("4. Add a new playlist");
+		System.out.println("5. Quit Music Library");
+		
+	}
+	
+	private void processMainMenu(int selectedOption) {
+		//TODO: Reorganize for better modularity
+		switch(selectedOption) {
+		case 1:
+			System.out.println("Which playlist would you like to play?");
+			int indexToPlay = selectPlaylist();
+			Playlist listToPlay = playlistHelper.getPlaylist(indexToPlay);
+			musicPlayer.setPlaylist(listToPlay);
+			musicPlayer.play(); //assumes shuffle/loop/normal option is asked in play();
+			runMenu();
+			break;
+		case 2:
+			System.out.println("Which playlist would you like to edit?");
+			int indexToEdit = selectPlaylist();
+			playlistHelper.edit(indexToEdit); //assumes the editing menu & functionality is in edit();
+			runMenu();
+			break;
+		case 3:
+			System.out.println("Which playlist would you like to delete?");
+			int indexToDelete = selectPlaylist();
+			playlistHelper.delete(indexToDelete); // delete() shouldn't delete All Songs or Favorites
+			runMenu();
+			break;
+		case 4:
+			playlistHelper.addNewPlaylist();
+			runMenu();
+			break;
+		case 5:
+			System.out.println("Thank you for using Music Library");
+			break;
+		default:
+			System.out.println("Please input a valid option");
+			int nextInput = this.getUserInput();
+			this.processMainMenu(nextInput);
+			break;
+		}
+	}
+	
+	private int selectPlaylist() {
+		playlistHelper.showPlaylists();
+		int playlistIndex = keyboardIn.nextInt();	
+		return playlistIndex;
+	}
+	
+	private int getUserInput() {
+		return this.keyboardIn.nextInt();
+	}
 }
